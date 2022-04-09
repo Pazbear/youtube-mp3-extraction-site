69970075ec417c44b64ac379d3629220586dec58
Pazbear <mkcho1997@daum.net>
2022-04-10 00:17:43 +0900

    crawl 일부 추가

---
+++ src/module/crawl.js
@@ -0,0 +1,49 @@
+const webdriver = require('selenium-webdriver');
+const { By } = require('selenium-webdriver')
+const chrome = require('selenium-webdriver/chrome')
+const path = require('path')
+const _ = require('lodash')
+
+const getVideoList = (driver) => {
+    //유튜브 로딩 대기  (3초)
+    return new Promise((resolve, reject) => {
+        setTimeout(async ()=>{
+            try{
+                const videoList = await driver.findElements(By.css('ytd-grid-video-renderer #thumbnail'))
+                resolve(await Promise.all(_.map(videoList, (video)=>{return video.getAttribute('href')})))
+            }catch(error){
+                reject(error)
+            }
+        },3000)
+    })
+}
+
+const crawl = async (youtube_channel_url) => {
+    //chromedriver 경로 설정
+    const service = new chrome.ServiceBuilder(path.join(__dirname, 'chromedriver.exe')).build();
+    chrome.setDefaultService(service);
+
+    //chrome 브라우저 빌드
+    const driver = await new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+    await driver.get(youtube_channel_url+'/videos')
+
+
+    await getVideoList(driver)
+    .then((list)=> {
+        
+        //현재 DB와 비교해서 없는 것들 추출
+    }).catch(error => {
+        console.error(error)
+    }).then(async ()=>{
+        console.log("bye driver")
+        await driver.quit();
+        process.exit(0)
+    })
+}
+
+module.exports = {
+    crawl
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const {crawl} = require('../module/crawl')
 
 router.get('/', (req, res, next)=>{
     res.render('index',{
         a:"hello",
         b:"안녕하세요"
     })
+})
+
+router.get('/crawl-test', async (req, res)=>{
+    await crawl("https://www.youtube.com/c/Sople")
+    res.send({success:true})
 })
 
 module.exports = router
